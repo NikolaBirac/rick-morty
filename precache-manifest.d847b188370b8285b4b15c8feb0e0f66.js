75146f812bf026cb4209c1fbacda44edc349d9de
NikolaBirac <nikolabirac61@gmail.com>
2019-08-20 14:57:40 +0200

    Updates

---
+++ precache-manifest.d847b188370b8285b4b15c8feb0e0f66.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac95f035b381314563ca384f7e1c206",
+    "url": "/rick-morty/index.html"
+  },
+  {
+    "revision": "dd03f2a3d19080741f02",
+    "url": "/rick-morty/static/css/2.cf796be5.chunk.css"
+  },
+  {
+    "revision": "64f23391b58da5dff87d",
+    "url": "/rick-morty/static/css/main.1799166f.chunk.css"
+  },
+  {
+    "revision": "dd03f2a3d19080741f02",
+    "url": "/rick-morty/static/js/2.3063188c.chunk.js"
+  },
+  {
+    "revision": "64f23391b58da5dff87d",
+    "url": "/rick-morty/static/js/main.9ff73e35.chunk.js"
+  },
+  {
+    "revision": "a079f2fc39b5e73e24f1",
+    "url": "/rick-morty/static/js/runtime~main.6bffb73a.js"
+  },
+  {
+    "revision": "c97d5a78efff1b32958647cfcc04e0b9",
+    "url": "/rick-morty/static/media/BentonSans.c97d5a78.otf"
+  },
+  {
+    "revision": "67cda19ead3e6cd1d8ce9ee3bc3eeafe",
+    "url": "/rick-morty/static/media/garbage.67cda19e.svg"
+  },
+  {
+    "revision": "686d74d658263f11ee4e556637707531",
+    "url": "/rick-morty/static/media/like.686d74d6.svg"
+  },
+  {
+    "revision": "13e479f7602ccff6c5a4b842ab5bda80",
+    "url": "/rick-morty/static/media/logo.13e479f7.png"
+  },
+  {
+    "revision": "af04b6e184fd7018c8ffe5f9655c1ff2",
+    "url": "/rick-morty/static/media/pozadina.af04b6e1.jpg"
+  },
+  {
+    "revision": "416ec287c4b08db1d72b0e38a6592a3c",
+    "url": "/rick-morty/static/media/something-went-wrong.416ec287.png"
+  },
+  {
+    "revision": "2eb430c7a22c016c3ce4ff20d0670370",
+    "url": "/rick-morty/static/media/unlike.2eb430c7.svg"
+  }
+]);
